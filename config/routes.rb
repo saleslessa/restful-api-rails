d2195c4c4e669354858a8767d097ccaefe243620
Sales Alencar <lopesdealencar@gmail.com>
2017-09-28 22:38:43 -0300

    .

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+  root to: "todos#index"
   resources :todos do
     resources :items
   end
 
-  root to: "todos#index"
 end
